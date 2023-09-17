30a1bb7c4139dc6b3fd3360d889ae5e4e9a37b22
Fabian T <htrevinomtz@gmail.com>
2023-09-17 11:26:33 -0600

    Basic working panel

--- clientapp/src/Components/Forecast.tsx
+++ clientapp/src/Components/Forecast.tsx
@@ -1,35 +1,61 @@
 import React from 'react';
-import { ForecastData } from '../Models/ForecastModel';
+import { ForecastData, Temperature, ValueItem } from '../Models/ForecastModel';
 
 interface ForecastProps {
-  forecast: ForecastData | null;
+    forecast: ForecastData | null;
 }
 
 const Forecast: React.FC<ForecastProps> = ({ forecast }) => {
-  if (!forecast) {
-    return null; // If there's no forecast data, don't render anything
-  }
+    if (!forecast) {
+        return null; // If there's no forecast data, don't render anything
+    }
 
-  return (
-    <div>
-      <h2>Forecast Data</h2>
-      <p>UpdateTime: {forecast.Properties.UpdateTime}</p>
-      <p>ValidTimes: {forecast.Properties.ValidTimes}</p>
+    // Helper function to group temperature values by day
+    const groupTemperaturesByDay = (temperatures: ValueItem[]) => {
+        const groupedTemperatures: { [day: string]: ValueItem[] } = {};
 
-      {/* Display other properties as needed */}
+        temperatures.forEach((item) => {
+            const day = item.ValidTime.split('T')[0]; // Extract the date part as the day
+            if (!groupedTemperatures[day]) {
+                groupedTemperatures[day] = [];
+            }
+            groupedTemperatures[day].push(item);
+        });
 
-      {/* Example for Temperature */}
-      <h3>Temperature</h3>
-      <p>UOM: {forecast.Properties.Temperature.uom}</p>
-      <ul>
-        {forecast.Properties.Temperature.Values.map((item, index) => (
-          <li key={index}>
-            ValidTime: {item.ValidTime}, Value: {item.Value}
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-};
+        return groupedTemperatures;
+    };
+
+    // Helper function to calculate the average temperature for a day
+    const calculateAverageTemperature = (temperatures: ValueItem[]) => {
+        if (temperatures.length === 0) {
+            return 0;
+        }
+
+        const total = temperatures.reduce((sum, temp) => sum + temp.Value, 0);
+        return total / temperatures.length;
+    };
+
+    // Group temperature values by day
+    const groupedTemperatures = groupTemperaturesByDay(
+        forecast.Properties.Temperature.Values
+    );
+
+    return (
+        <div>
+            <h2>Forecast Data</h2>
+            <p>UpdateTime: {forecast.Properties.UpdateTime}</p>
+
+            <h3>Temperature</h3>
+            <div className="temperature-grid">
+                {Object.entries(groupedTemperatures).map(([day, temperatures], index) => (
+                    <div key={index} className="temperature-panel">
+                        <h4>Day: {day}</h4>
+                        <p>Avg Temperature: {calculateAverageTemperature(temperatures).toFixed(2)} °C</p>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}
 
 export default Forecast;
