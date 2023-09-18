7d73c15602b49a53eb05438de17bab8679ac60c3
Fabian T <htrevinomtz@gmail.com>
2023-09-17 21:23:01 -0600

    Readable date

--- clientapp/src/Components/WeatherDisplay.tsx
+++ clientapp/src/Components/WeatherDisplay.tsx
@@ -6,17 +6,38 @@ interface WeatherDisplayProps {
 }
 
 export const WeatherDisplay: React.FC<WeatherDisplayProps> = ({ weatherDataArray }) => {
-  // Slice the array to display up to 7 objects
-  const displayedWeatherData = weatherDataArray.slice(0, 7);
+  // Helper function to render a property if it's a non-zero number
+  const renderProperty = (key: string, value: number | string) => {
+    if (typeof value === 'number' && value !== 0) {
+      return (
+        <p key={key}>
+          {key}: {value.toFixed(2)}
+        </p>
+      );
+    }
+    return null;
+  };
+
+  // Helper function to format the date to a human-readable format
+  const formatReadableDate = (dateString: string) => {
+    const date = new Date(dateString);
+    const options: Intl.DateTimeFormatOptions = {
+      weekday: 'long',
+      month: 'long',
+      day: 'numeric',
+    };
+    return date.toLocaleDateString('en-US', options);
+  };
 
   return (
     <div>
       <h2>Weather Data Display</h2>
-      {displayedWeatherData.map((weatherData, index) => (
+      {weatherDataArray.map((weatherData, index) => (
         <div key={index} className="weather-item">
-          <h3>Date: {weatherData.Date}</h3>
-          <p>Avg Temperature: {weatherData.TemperatureAverage.toFixed(2)} °C</p>
-          {/* Display other weather data properties as needed */}
+          <h3>Date: {formatReadableDate(weatherData.Date)}</h3>
+          {Object.entries(weatherData).map(([key, value]) => (
+            <React.Fragment key={key}>{renderProperty(key, value)}</React.Fragment>
+          ))}
         </div>
       ))}
     </div>
